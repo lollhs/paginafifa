2bada323e41288927be40af419e26be537a18500
carolina florez <carollohe83@gmail.com>
2026-03-06 19:37:55 -0500

    fix test script

--- ui-testing-clase/Ejercicio1.spec.ts
+++ ui-testing-clase/Ejercicio1.spec.ts
@@ -1,39 +1,22 @@
-import { test, expect } from '@playwright/test'; 
+import { test, expect } from '@playwright/test';
 
- 
+test('formulario de login: validación de campos requeridos', async ({ page }) => {
+  await page.goto('./67001167/index.html');
 
-test('formulario de login: validación de campos requeridos', async ({ page }) => { 
+  await page.click('button[type="submit"]');
 
-  await page.goto('http://127.0.0.1:5500/67001167/index.html'); 
+  await expect(page.getByText('El correo es obligatorio')).toBeVisible();
+  await expect(page.getByText('La contraseña es obligatoria')).toBeVisible();
+});
 
- 
+test('formulario de login: credenciales válidas', async ({ page }) => {
+  await page.goto('./67001167/index.html');
 
-  await page.click('button[type="submit"]'); 
+  await page.fill('#email', 'usuario@test.com');
+  await page.fill('#password', '123456');
 
- 
+  await page.click('button[type="submit"]');
 
-  await expect(page.getByText('El correo es obligatorio')).toBeVisible(); 
-
-  await expect(page.getByText('La contraseña es obligatoria')).toBeVisible(); 
-
-}); 
-
- 
-
-test('formulario de login: credenciales válidas', async ({ page }) => { 
-
-  await page.goto('http://127.0.0.1:5500/67001167/index.html'); 
-
- 
-
-  await page.fill('#email', 'usuario@test.com'); 
-
- 
-
- 
-
-  await expect(page).toHaveURL(/.*dashboard/); 
-
-  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible(); 
-
-}); 
+  await expect(page).toHaveURL(/.*dashboard/);
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+});
